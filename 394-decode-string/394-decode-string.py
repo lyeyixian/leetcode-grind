f34bca89340c00d5580b926276df9ec68c104cad
Lye Yi Xian <58477697+lyeyixian@users.noreply.github.com>
2022-04-04 00:29:13 +0800

    Time: 28 ms (94.77%), Space: 13.8 MB (74.57%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,27 +1,30 @@
+# - iterative solution using stack
+# - push into stack until closing bracket
+#     - get the substring enclosed inside the bracket
+#         - pop the stack and store the substring until opening bracket
+#     - get the digit
+#         - pop until it is not digit
+#         - store the digit
+#     - append substring * digit back to the stack
+# - string join the stack
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
         
         for character in s:
             if (character == ']'):
-                popOfStack = stack.pop()
                 res = ''
                 
-                while popOfStack != '[':
-                    res = popOfStack + res
-                    popOfStack = stack.pop()
+                while stack[-1] != '[':
+                    res = stack.pop() + res
                     
-                topOfStack = stack[-1]
+                stack.pop()
                 num = ''
                 
-                while topOfStack.isdigit():
+                while stack and stack[-1].isdigit():
                     num = stack.pop() + num
-                    topOfStack = stack[-1] if len(stack) > 0 else ''
                     
-                res *= int(num)
-                
-                for c in res:
-                    stack.append(c)
+                stack.append(res * int(num))
             else:
                 stack.append(character)
                 
