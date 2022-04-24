6dd4ae43a1daade275faaffde98a2aa9c2e3664a
Lye Yi Xian <58477697+lyeyixian@users.noreply.github.com>
2022-04-24 16:13:05 +0800

    Time: 81 ms (53.17%), Space: 42.5 MB (33.65%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const parenMapping = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+    const openParenList = ['(', '[', '{']
+    const stack = []
+    
+    for (const char of s) {
+        if (openParenList.includes(char)) {
+            const closeParen = parenMapping[char]
+            
+            stack.push(closeParen)
+        } else {
+            if (!stack.length) {
+                return false
+            }
+            
+            const poppedParen = stack.pop()
+            
+            if (poppedParen !== char) {
+                return false
+            }
+        }
+    }
+    
+    return stack.length ? false : true
+};
