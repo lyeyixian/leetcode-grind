2eafe0792faa2763852d839522d2d3e7307fadd4
Lye Yi Xian <58477697+lyeyixian@users.noreply.github.com>
2022-04-24 17:14:08 +0800

    Time: 106 ms (29.31%), Space: 43.8 MB (94.17%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -10,26 +10,64 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
+// Iterative Solution
 var mergeTwoLists = function(list1, list2) {
     if (!list1 && !list2) {
         return null
     }
     
-    if (!list1) {
-        return list2
+    const dummy = {
+        val: 0,
+        next: null
+    }
+    let ptr = dummy
+    let ptr1 = list1
+    let ptr2 = list2
+    
+    while (ptr1 && ptr2) {
+        if (ptr1.val <= ptr2.val) {
+            ptr.next = ptr1
+            ptr1 = ptr1.next
+        } else {
+            ptr.next = ptr2
+            ptr2 = ptr2.next
+        }
+        
+        ptr = ptr.next
     }
     
-    if (!list2) {
-        return list1
+    if (ptr1) {
+        ptr.next = ptr1
     }
     
-    if (list1.val <= list2.val) {
-        list1.next = mergeTwoLists(list1.next, list2)
-        
-        return list1
-    } else {
-        list2.next = mergeTwoLists(list1, list2.next)
-        
-        return list2
+    if (ptr2) {
+        ptr.next = ptr2
     }
+    
+    return dummy.next
 };
+
+// Recursive Solution
+// var mergeTwoLists = function(list1, list2) {
+//     if (!list1 && !list2) {
+//         return null
+//     }
+    
+//     if (!list1) {
+//         return list2
+//     }
+    
+//     if (!list2) {
+//         return list1
+//     }
+    
+//     if (list1.val <= list2.val) {
+//         list1.next = mergeTwoLists(list1.next, list2)
+        
+//         return list1
+//     } else {
+//         list2.next = mergeTwoLists(list1, list2.next)
+        
+//         return list2
+//     }
+// };
