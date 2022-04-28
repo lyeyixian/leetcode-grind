5b82bef17f5a19a81e90785623eaed8850082a92
Lye Yi Xian <58477697+lyeyixian@users.noreply.github.com>
2022-04-28 23:06:23 +0800

    Time: 77 ms (81.18%), Space: 47.3 MB (33.43%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,6 +3,14 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    function isAlphanumeric(char) {
+        const codeOfChar = char.charCodeAt(0)
+        
+        return codeOfChar >= 'a'.charCodeAt(0) && codeOfChar <= 'z'.charCodeAt(0) ||
+            codeOfChar >= 'A'.charCodeAt(0) && codeOfChar <= 'Z'.charCodeAt(0) ||
+            codeOfChar >= '0'.charCodeAt(0) && codeOfChar <= '9'.charCodeAt(0)
+    }
+    
     let left = 0
     let right = s.length - 1
     
@@ -10,12 +18,12 @@ var isPalindrome = function(s) {
         const leftChar = s[left]
         const rightChar = s[right]
         
-        if (!/[0-9a-zA-Z]+/.test(leftChar)) {
+        if (!isAlphanumeric(leftChar)) {
             left++
             continue
         }
         
-        if (!/[0-9a-zA-Z]+/.test(rightChar)) {
+        if (!isAlphanumeric(rightChar)) {
             right--
             continue
         }
