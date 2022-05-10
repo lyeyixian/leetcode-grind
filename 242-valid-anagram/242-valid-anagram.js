b90ad0076babe4712357bf33f81ec78c197f2c86
Lye Yi Xian <58477697+lyeyixian@users.noreply.github.com>
2022-05-10 22:21:00 +0800

    Time: 92 ms (74.26%), Space: 44.2 MB (55.03%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,24 +4,52 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    const countArr = []
-    const codeOfA = 'a'.charCodeAt(0)
+    const table = {}
     
-    for (let i = 0; i < 26; i++) {
-        countArr.push(0)
-    }
-    
-    for (const c of s) {
-        const idx = c.charCodeAt(0)
+    for (const char of s) {
+        if (!table[char]) {
+            table[char] = 0
+        }
         
-        countArr[idx - codeOfA]++
+        table[char]++
     }
     
-    for (const c of t) {
-        const idx = c.charCodeAt(0)
+    for (const char of t) {
+        if (!table[char]) {
+            return false
+        }
         
-        countArr[idx - codeOfA]--
+        table[char]--
     }
     
-    return countArr.every(count => count === 0)
+    for (const key in table) {
+        if (table[key] !== 0) {
+            return false
+        }
+    }
+    
+    return true
 };
+
+// var isAnagram = function(s, t) {
+//     const countArr = []
+//     const codeOfA = 'a'.charCodeAt(0)
+    
+//     for (let i = 0; i < 26; i++) {
+//         countArr.push(0)
+//     }
+    
+//     for (const c of s) {
+//         const idx = c.charCodeAt(0)
+        
+//         countArr[idx - codeOfA]++
+//     }
+    
+//     for (const c of t) {
+//         const idx = c.charCodeAt(0)
+        
+//         countArr[idx - codeOfA]--
+//     }
+    
+//     return countArr.every(count => count === 0)
+// };
