a59276f4490bc41f30b52c0b2f44809084aafde8
Lye Yi Xian <58477697+lyeyixian@users.noreply.github.com>
2022-05-14 22:54:27 +0800

    Time: 99 ms (48.67%), Space: 44.6 MB (27.43%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -6,26 +6,57 @@
  * @return {number[][]}
  */
 var floodFill = function(image, sr, sc, newColor) {
-    const fixedColor = image[sr][sc]
-    
     if (image[sr][sc] === newColor) {
         return image
     }
     
-    function fill(row, col) {
-        if (row < 0 || col < 0 || row >= image.length || col >= image[0].length || image[row][col] !== fixedColor) {
-            return
-        }
-        
-        image[row][col] = newColor
-        
-        fill(row + 1, col)
-        fill(row - 1, col)
-        fill(row, col + 1)
-        fill(row, col - 1)
-    }
+    const DIRECTION = [0, 1, 0, -1, 0]
+    const fixedColor = image[sr][sc]
+    const queue = []
     
-    fill(sr, sc)
+    image[sr][sc] = newColor
+    queue.push([sr, sc])
+    
+    while (queue.length) {
+        const current = queue.shift()
+        
+        for (let i = 0; i < DIRECTION.length - 1; i++) {
+            const row = current[0] + DIRECTION[i]
+            const col = current[1] + DIRECTION[i + 1]
+            
+            if (row < 0 || col < 0 || row >= image.length || col >= image[0].length || image[row][col] !== fixedColor) {
+                continue
+            }
+            
+            image[row][col] = newColor
+            queue.push([row, col])
+        }
+    }
     
     return image
 };
+
+// var floodFill = function(image, sr, sc, newColor) {
+//     const fixedColor = image[sr][sc]
+    
+//     if (image[sr][sc] === newColor) {
+//         return image
+//     }
+    
+//     function fill(row, col) {
+//         if (row < 0 || col < 0 || row >= image.length || col >= image[0].length || image[row][col] !== fixedColor) {
+//             return
+//         }
+        
+//         image[row][col] = newColor
+        
+//         fill(row + 1, col)
+//         fill(row - 1, col)
+//         fill(row, col + 1)
+//         fill(row, col - 1)
+//     }
+    
+//     fill(sr, sc)
+    
+//     return image
+// };
