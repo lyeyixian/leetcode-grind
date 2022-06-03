da4c25bfd6a73ce80a708547b3600e966a8d095d
Lye Yi Xian <58477697+lyeyixian@users.noreply.github.com>
2022-06-03 22:30:44 +0800

    Time: 701 ms (10.25%), Space: 57.4 MB (79.06%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -3,11 +3,67 @@
  * @param {number} k
  * @return {number[][]}
  */
+// Sorting
+// var kClosest = function(points, k) {
+//     points.sort((a, b) => distanceToOrigin(a) - distanceToOrigin(b))
+    
+//     return points.slice(0, k)
+// };
+//
+// Max Heap/PQ
+// var kClosest = function(points, k) {
+//     const pq = new MaxPriorityQueue(point => distanceToOrigin(point))
+    
+//     points.forEach(point => {
+//         if (pq.size() === k) {
+//             if (distanceToOrigin(point) < distanceToOrigin(pq.front())) {
+//                 pq.dequeue()
+//                 pq.enqueue(point)
+//             }
+//         } else {
+//             pq.enqueue(point)
+//         }
+//     })
+    
+//     return pq.toArray()
+// };
+
 var kClosest = function(points, k) {
-    points.sort((a, b) => distanceToOrigin(a) - distanceToOrigin(b))
+    let left = 0
+    let right = points.length - 1
+    let partitionIndex = points.length
+    
+    while (partitionIndex !== k) {
+        partitionIndex = partition(points, left, right)
+        
+        if (partitionIndex < k) {
+            left = partitionIndex + 1
+        } else {
+            right = partitionIndex - 1
+        }
+    }
     
     return points.slice(0, k)
 };
+
+function partition(points, left, right) {
+    const pivot = points[right]
+    let pivotIndex = left
+    
+    for (let i = left; i <= right; i++) {
+        if (distanceToOrigin(points[i]) < distanceToOrigin(pivot)) {
+            const temp = points[i]
+            points[i] = points[pivotIndex]
+            points[pivotIndex] = temp
+            pivotIndex++
+        }
+    }
+
+    points[right] = points[pivotIndex]
+    points[pivotIndex] = pivot
+    
+    return pivotIndex
+}
 
 function distanceToOrigin(point) {
     return Math.sqrt(point[0] * point[0] + point[1] * point[1])
