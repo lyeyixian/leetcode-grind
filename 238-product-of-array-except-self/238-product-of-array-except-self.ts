db4d217f7d50397dcd14727124ed2d97d72b4d94
Lye Yi Xian <58477697+lyeyixian@users.noreply.github.com>
2022-08-09 23:47:49 +0800

    Time: 141 ms (77.91%), Space: 53.3 MB (83.73%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.ts
+++ 238-product-of-array-except-self/238-product-of-array-except-self.ts
@@ -1,25 +1,42 @@
 function productExceptSelf(nums: number[]): number[] {
-    const fromLeft = []
-    
-    fromLeft.push(1)
-    
-    for (let i = 1; i < nums.length; i++) {
-        fromLeft[i] = fromLeft[i - 1] * nums[i - 1]
-    }
-    
-    const fromRight = new Array(nums.length).fill(0)
-    
-    fromRight[fromRight.length - 1] = 1
-    
-    for (let i = nums.length - 2; i >= 0; i--) {
-        fromRight[i] = fromRight[i + 1] * nums[i + 1]
-    }
-    
     const res = []
     
-    for (let i = 0; i < nums.length; i++) {
-        res[i] = fromLeft[i] * fromRight[i]
+    res.push(1)
+    
+    for (let i = 1; i < nums.length; i++) {
+        res[i] = res[i - 1] * nums[i - 1]
+    }
+    
+    let fromRight = 1
+    
+    for (let i = nums.length - 1; i >= 0; i--) {
+        res[i] *= fromRight
+        fromRight *= nums[i]
     }
     
     return res
 };
+
+//     const fromLeft = []
+
+//     fromLeft.push(1)
+
+//     for (let i = 1; i < nums.length; i++) {
+//         fromLeft[i] = fromLeft[i - 1] * nums[i - 1]
+//     }
+
+//     const fromRight = new Array(nums.length).fill(0)
+
+//     fromRight[fromRight.length - 1] = 1
+
+//     for (let i = nums.length - 2; i >= 0; i--) {
+//         fromRight[i] = fromRight[i + 1] * nums[i + 1]
+//     }
+
+//     const res = []
+
+//     for (let i = 0; i < nums.length; i++) {
+//         res.push(fromLeft[i] * fromRight[i])
+//     }
+
+//     return res
