3e716717d9f55648988f7eafdd30506977a33860
Lye Yi Xian <58477697+lyeyixian@users.noreply.github.com>
2022-09-01 23:37:30 +0800

    Time: 142 ms (31.88%), Space: 44.9 MB (52.35%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -3,23 +3,40 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    const rowToZero = new Set()
-    const colToZero = new Set()
+    let firstColIsZero = false
     
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[0].length; j++) {
             if (matrix[i][j] === 0) {
-                rowToZero.add(i)
-                colToZero.add(j)
+                matrix[i][0] = 0
+                
+                if (j === 0) {
+                    firstColIsZero = true
+                } else {
+                    matrix[0][j] = 0   
+                }
             }
         }
     }
     
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[0].length; j++) {
-            if (rowToZero.has(i) || colToZero.has(j)) {
+    for (let i = 1; i < matrix.length; i++) {
+        for (let j = 1; j < matrix[0].length; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
                 matrix[i][j] = 0
             }
         }
     }
+    
+    if (matrix[0][0] === 0) {
+        for (let i = 0; i < matrix[0].length; i++) {
+            matrix[0][i] = 0
+        }
+    }
+    
+    if (firstColIsZero) {
+        for (let i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 0
+        }
+    }
 };
+
