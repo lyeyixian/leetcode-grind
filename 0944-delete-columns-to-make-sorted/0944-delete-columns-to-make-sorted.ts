48e9949daf82ed8b02960a69dc28d090a963f03c
Lye Yi Xian <lyeyx999@hotmail.com>
2023-01-03 22:32:10 +0800

    Time: 131 ms (37.50%), Space: 46.9 MB (31.25%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.ts
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.ts
@@ -2,14 +2,14 @@ function minDeletionSize(strs: string[]): number {
     const wordLength = strs[0].length
     let numOfCol = 0
     
-    // loop through col by col
+    // loop through col
     for (let col = 0; col < wordLength; col++) {
-        // loop through the char of that col of each str in strs
+        // loop through row and compare with previous row
         for (let row = 1; row < strs.length; row++) {
-            const currentStr = strs[row]
-            const prevStr = strs[row - 1]
+            const currentChar = strs[row][col]
+            const prevChar = strs[row - 1][col]
             
-            if (currentStr[col] < prevStr[col]) {
+            if (currentChar < prevChar) {
                 numOfCol++
                 break
             }
