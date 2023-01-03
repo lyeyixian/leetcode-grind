11b492b76227cce170bff6a464d8ddeb0adb8acc
Lye Yi Xian <lyeyx999@hotmail.com>
2023-01-03 21:37:30 +0800

    Time: 62 ms (87.56%), Space: 42.9 MB (70.47%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.ts
+++ 0290-word-pattern/0290-word-pattern.ts
@@ -2,10 +2,13 @@ function wordPattern(pattern: string, s: string): boolean {
     const map = new Map()
     const split = s.split(' ')
 
+    // make sure the length of words and pattern are the same before proceeding
     if (split.length !== pattern.length) {
         return false
     }
 
+    // put word in Map.
+    // If word already in Map, check if they are the same word, return false if not
     for (let i = 0; i < split.length; i++) {
         const patternChar = pattern[i]
         const word = split[i]
@@ -21,6 +24,8 @@ function wordPattern(pattern: string, s: string): boolean {
         }
     }
 
+    // cannot have same word on different letter
+    // ex: abcd --> aaaa aaaa cccc dddd (wrong)
     const set = new Set()
 
     for (const [key, value] of map.entries()) {
