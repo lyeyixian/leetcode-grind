b86ebf37d5865b05d26504233f6908852b6cb82e
Lye Yi Xian <lyeyx999@hotmail.com>
2023-01-04 17:34:27 +0800

    Time: 215 ms (81.82%), Space: 62.6 MB (54.55%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.ts
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.ts
@@ -1,6 +1,7 @@
 function minimumRounds(tasks: number[]): number {
     const map = new Map()
     
+    // count the frequency
     for (const level of tasks) {
         if (!map.has(level)) {
             map.set(level, 0)
@@ -12,15 +13,21 @@ function minimumRounds(tasks: number[]): number {
     let round = 0
     
     for (const freq of map.values()) {
+        // because can only pick 2 or 3, cant pick 1
         if (freq === 1) {
             return -1
         }
         
-        const remainder = freq % 3
-        
-        if (remainder === 0) {
+        // freq = 3k
+        if (freq % 3 === 0) {
             round += freq / 3
         } else {
+            // freq = 3k + 1
+            // ex: 7 = 3 3 --> 3 2 2
+            //
+            // freq = 3k + 2
+            // ex: 8 = 3 3 --> 3 3 2
+            // both case need to add 1
             round += Math.floor(freq / 3) + 1
         }
     }
