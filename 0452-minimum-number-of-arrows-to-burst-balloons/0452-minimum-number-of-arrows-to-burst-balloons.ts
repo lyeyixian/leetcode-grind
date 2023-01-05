94b83710081dc814fa57de04d2b961ccc67cc8a6
Lye Yi Xian <lyeyx999@hotmail.com>
2023-01-05 18:53:40 +0800

    Time: 464 ms (26.32%), Space: 74.5 MB (26.32%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.ts
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.ts
@@ -6,9 +6,11 @@ function findMinArrowShots(points: number[][]): number {
     points.sort((a, b) => a[1] - b[1])
     
     let arrowCount = 1
-    let shotPos = points[0][1]
+    let shotPos = points[0][1] // greedily choose to shot at the rightmost pos of a balloon
     
     for (let i = 1; i < points.length; i++) {
+        // skip the current if the shot covered this balloon.
+        // only need to check this as the array is sorted according to rightmost pos
         if (shotPos >= points[i][0]) {
             continue
         }
