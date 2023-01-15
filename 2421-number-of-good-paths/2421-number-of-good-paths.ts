57880b5e8ffafe1e8a6fd51ff3f048ce29aad8ab
Lye Yi Xian <lyeyx999@hotmail.com>
2023-01-16 00:17:47 +0800

    Time: 926 ms (40.00%), Space: 89.3 MB (100.00%) - LeetHub

---
+++ 2421-number-of-good-paths/2421-number-of-good-paths.ts
@@ -0,0 +1,83 @@
+function numberOfGoodPaths(vals: number[], edges: number[][]): number {
+    const adjList = []
+    
+    for (let i = 0; i < vals.length; i++) {
+        adjList.push([])
+    }
+    
+    for (const [u, v] of edges) {
+        adjList[u].push(v)
+        adjList[v].push(u)
+    }
+    
+    const map = new Map()
+    const transformedVals = vals.map((val, index) => ({ val, node: index }))
+    
+    transformedVals.sort((a, b) => a.val - b.val)
+    
+    for (const transformedVal of transformedVals) {
+        const val = transformedVal.val
+        const node = transformedVal.node
+        
+        if (!map.has(val)) {
+            map.set(val, [])
+        }
+        
+        map.get(val).push(node)
+    }
+    
+    const uf = []
+    
+    for (let i = 0; i < vals.length; i++) {
+        uf[i] = i
+    }
+    
+    let res = vals.length
+    
+    for (const nodes of map.values()) {
+        for (const node of nodes) {
+            for (const neighbour of adjList[node]) {
+                if (vals[neighbour] <= vals[node]) {
+                    union(uf, node, neighbour)
+                }
+            }
+        }
+        
+        const counter = new Map()
+        
+        for (const node of nodes) {
+            const key = find(uf, node)
+            
+            if (!counter.has(key)) {
+                counter.set(key, 0)
+            }
+            
+            counter.set(key, counter.get(key) + 1)
+        }
+        
+        for (const count of counter.values()) {
+            if (count >= 2) {
+                res += Math.floor(count * (count - 1) / 2)
+            }
+        }
+    }
+    
+    return res
+};
+
+function union(uf, a, b) {
+    const rootA = find(uf, a)
+    const rootB = find(uf, b)
+    
+    if (rootA !== rootB) {
+        uf[rootA] = rootB    
+    }
+}
+
+function find(uf, a) {
+    if (uf[a] !== a) {
+        uf[a] = find(uf, uf[a])
+    }
+    
+    return uf[a]
+}
