0e540d353f73c02e90b183aa57d8ba35acadaac7
Lye Yi Xian <lyeyx999@hotmail.com>
2023-01-25 23:45:59 +0800

    Time: 657 ms (25.00%), Space: 94 MB (25.00%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.ts
@@ -0,0 +1,53 @@
+function closestMeetingNode(edges: number[], node1: number, node2: number): number {
+    const adjList = []
+    
+    for (let i = 0; i < edges.length; i++) {
+        adjList[i] = []
+        
+        const dest = edges[i]
+        
+        if (dest > -1) {
+            adjList[i].push(dest)
+        }
+    }
+    
+    const node1DistMap = new Map()
+    const node2DistMap = new Map()
+    
+    bfs(node1, node1DistMap, adjList)
+    bfs(node2, node2DistMap, adjList)
+    
+    let resIndex = -1
+    let minDistance = Infinity
+    
+    for (let i = 0; i < edges.length; i++) {
+        if (node1DistMap.has(i) && node2DistMap.has(i)) {
+            const maxDist = Math.max(node1DistMap.get(i), node2DistMap.get(i))
+            
+            if (maxDist < minDistance) {
+                resIndex = i
+                minDistance = maxDist
+            }
+        }
+    }
+    
+    return resIndex
+};
+
+function bfs(src, nodeDistMap, adjList) {
+    const queue = []
+    
+    queue.push({ node: src, distance: 0 })
+    nodeDistMap.set(src, 0)
+    
+    while (queue.length) {
+        const { node, distance } = queue.shift()
+        
+        for (const neighbour of adjList[node]) {
+            if (!nodeDistMap.has(neighbour)) {
+                queue.push({ node: neighbour, distance: distance + 1 })
+                nodeDistMap.set(neighbour, distance + 1)    
+            }
+        }
+    }
+}
