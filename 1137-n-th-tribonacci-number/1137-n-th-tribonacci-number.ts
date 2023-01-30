738bd1bae3a26a9b4ed6744a79f3aa4462f5262f
Lye Yi Xian <lyeyx999@hotmail.com>
2023-01-30 15:45:08 +0800

    Time: 71 ms (62.13%), Space: 43.3 MB (22.49%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.ts
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.ts
@@ -4,28 +4,9 @@ function tribonacci(n: number): number {
     mem[1] = 1
     mem[2] = 1
     
-    // return helper(n, mem)
-    
     for (let i = 3; i <= n; i++) {
         mem[i] = mem[i - 3] + mem[i - 2] + mem[i - 1]
     }
         
     return mem[n]
 };
-
-function helper(n, mem) {
-    if (mem[n] !== -1) {
-        return mem[n]
-    }
-    
-//     if (n === 0) {
-//         return 0
-//     }
-    
-//     if (n === 1 || n === 2) {
-//         return 1
-//     }
-    mem[n] = helper(n - 3, mem) + helper(n - 2, mem) + helper(n - 1, mem)
-    
-    return mem[n]
-}
