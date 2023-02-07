12ebb95d6b1e9eb07eb67abe508402448f835879
Lye Yi Xian <lyeyx999@hotmail.com>
2023-02-07 14:32:38 +0800

    Time: 132 ms (66.07%), Space: 54.4 MB (71.43%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.ts
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.ts
@@ -6,8 +6,14 @@ function totalFruit(fruits: number[]): number {
     for (let right = 0; right < fruits.length; right++) {
         const fruit = fruits[right]
         
+        if (!map.has(fruit)) {
+            map.set(fruit, 0)
+        }
+        
+        map.set(fruit, map.get(fruit) + 1)
+        
         // maintain the invariant of map size <= 2
-        while (map.size >= 2 && !map.has(fruit)) {
+        while (map.size > 2) {
             const fruitToEvict = fruits[left]
 
             map.set(fruitToEvict, map.get(fruitToEvict) - 1)
@@ -18,11 +24,6 @@ function totalFruit(fruits: number[]): number {
             }
         }
         
-        if (!map.has(fruit)) {
-            map.set(fruit, 0)
-        }
-        
-        map.set(fruit, map.get(fruit) + 1)
         globalMax = Math.max(globalMax, right - left + 1)
     }
     
