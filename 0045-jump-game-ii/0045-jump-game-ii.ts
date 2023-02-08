0ef2bedcde937300d4f68bbf86787e418c39ed83
Lye Yi Xian <lyeyx999@hotmail.com>
2023-02-08 15:43:57 +0800

    Time: 73 ms (86.56%), Space: 45.4 MB (50.54%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.ts
+++ 0045-jump-game-ii/0045-jump-game-ii.ts
@@ -1,17 +1,39 @@
 function jump(nums: number[]): number {
-    const dp = new Array(nums.length).fill(Infinity)
+//     const dp = new Array(nums.length).fill(Infinity)
     
-    dp[0] = 0
+//     dp[0] = 0
     
-    for (let i = 0; i < nums.length; i++) {
-        const numJump = nums[i]
+//     for (let i = 0; i < nums.length; i++) {
+//         const numJump = nums[i]
         
-        for (let j = 1; j <= numJump && i + j < nums.length; j++) {
-            const nextIdx = i + j
+//         for (let j = 1; j <= numJump && i + j < nums.length; j++) {
+//             const nextIdx = i + j
             
-            dp[nextIdx] = Math.min(dp[nextIdx], dp[i] + 1)
+//             dp[nextIdx] = Math.min(dp[nextIdx], dp[i] + 1)
+//         }
+//     }
+    
+//     return dp[nums.length - 1]
+    return jump2(nums)
+};
+
+// non-dp solution
+function jump2(nums: number[]): number {
+    let res = 0
+    let left = 0
+    let right = 0
+    
+    while (right < nums.length - 1) {
+        let farthestCanReach = 0
+        
+        for (let i = left; i <= right; i++) {
+            farthestCanReach = Math.max(farthestCanReach, i + nums[i])
         }
+        
+        left = right + 1
+        right = farthestCanReach
+        res++
     }
     
-    return dp[nums.length - 1]
-};
+    return res
+}
