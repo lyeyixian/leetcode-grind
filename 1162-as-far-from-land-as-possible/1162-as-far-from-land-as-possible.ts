d82e8abc6e7ab21da3a84c71b9fdf002f6b06ccb
Lye Yi Xian <lyeyx999@hotmail.com>
2023-02-10 15:47:45 +0800

    Time: 131 ms (85.71%), Space: 50.2 MB (66.67%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.ts
@@ -0,0 +1,35 @@
+function maxDistance(grid: number[][]): number {
+    const queue = []
+    const n = grid.length
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1) {
+                queue.push([i, j])
+            }
+        }
+    }
+    
+    let res = -1
+    const direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    
+    while (queue.length) {
+        const [x, y] = queue.shift()
+        
+        res = grid[x][y]
+        
+        for (const [dx, dy] of direction) {
+            const newX = x + dx
+            const newY = y + dy
+            
+            if (newX < 0 || newY < 0 || newX >= n || newY >= n || grid[newX][newY] !== 0) {
+                continue
+            }
+            
+            queue.push([newX, newY])
+            grid[newX][newY] = grid[x][y] + 1
+        }
+    }
+    
+    return res > 1 ? res - 1 : -1
+};
