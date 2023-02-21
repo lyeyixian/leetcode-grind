0b70391e0169a93372f5eba0bbcb4e93b7608c8a
Lye Yi Xian <lyeyx999@hotmail.com>
2023-02-21 15:49:39 +0800

    Time: 72 ms (51.85%), Space: 46.2 MB (44.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.ts
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.ts
@@ -1,12 +1,21 @@
 function singleNonDuplicate(nums: number[]): number {
-    for (let i = 0; i < nums.length; i++) {
-        const num1 = nums[i]
-        const num2 = nums[i + 1]
+    let left = 0
+    let right = nums.length
+    
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2)
+        const currNum = nums[mid]
         
-        if (num1 !== num2) {
-            return num1
+        if (currNum !== nums[mid - 1] && currNum !== nums[mid + 1]) {
+            return currNum
         }
         
-        i++
+        const leftSize = currNum === nums[mid - 1] ? mid - 1 : mid
+        
+        if (leftSize % 2) {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
     }
 };
