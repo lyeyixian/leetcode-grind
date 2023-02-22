ea200679a365504945250bd9f29141d4448e092a
Lye Yi Xian <lyeyx999@hotmail.com>
2023-02-22 14:20:15 +0800

    Time: 89 ms (66.67%), Space: 46.7 MB (16.67%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.ts
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.ts
@@ -1,13 +1,23 @@
 function shipWithinDays(weights: number[], days: number): number {
-    let left = Math.max(...weights)
-    let right = weights.reduce((a, b) => a + b)
+    let maxLoad = 0
+    let totalLoad = 0
+    
+    for (const weight of weights) {
+        totalLoad += weight
+        maxLoad = Math.max(weight, maxLoad)
+    }
+    
+    let left = maxLoad
+    let right = totalLoad
 
     while (left < right) {
         const mid = Math.floor((left + right) / 2)
         
         if (canShip(mid, weights, days)) {
+            // current mid can still be the answer, so keep it
             right = mid
         } else {
+            // current mid is definitely not the answer, so skip it
             left = mid + 1
         }
     }
