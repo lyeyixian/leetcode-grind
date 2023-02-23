55c8a64480fb890d293e643f19c1e9ff35542e69
Lye Yi Xian <lyeyx999@hotmail.com>
2023-02-23 16:09:01 +0800

    Time: 136 ms (25.05%), Space: 114.6 MB (5.12%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,33 +1,31 @@
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        // min pq to get the min capital in log n time
         PriorityQueue<int[]> minCapital = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        
         for (int i = 0; i < profits.length; i++) {
             minCapital.offer(new int[]{ profits[i], capital[i] });
         }
         
+        // max pq to greedily pick the max profit that the current capital allow us to
         PriorityQueue<Integer> maxProfit = new PriorityQueue<>(Collections.reverseOrder());
         
         int resCapital = w;
         
         for (int i = 0; i < k; i++) {
-            fillPq(maxProfit, resCapital, profits, capital, minCapital);
+            // starting from the lowest, put all possible profit that holds for current capital into max profit pq
+            while (!minCapital.isEmpty() && minCapital.peek()[1] <= resCapital) {
+                maxProfit.offer(minCapital.poll()[0]);
+            }
             
+            // do this loop for k times or until max profit pq is empty
             if (maxProfit.isEmpty()) {
                 break;
             }
             
-            int currentProfit = maxProfit.poll();
-            
-            resCapital += currentProfit;
+            // get the max profit currently from max pq
+            resCapital += maxProfit.poll();
         }
         
         return resCapital;
-    }
-    
-    private void fillPq(PriorityQueue<Integer> maxProfit, int currentCapital, int[] profits, int[] capital, PriorityQueue<int[]> minCapital) {
-        while (!minCapital.isEmpty() && minCapital.peek()[1] <= currentCapital) {
-            maxProfit.offer(minCapital.poll()[0]);
-        }
     }
 }
