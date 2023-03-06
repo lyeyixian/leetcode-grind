acb9a373a62e6a9eaaf1609d56d4b9e77d2579ed
Lye Yi Xian <lyeyx999@hotmail.com>
2023-03-06 12:34:50 +0800

    Time: 75 ms (62.86%), Space: 44.9 MB (17.14%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.ts
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.ts
@@ -1,16 +1,32 @@
 function findKthPositive(arr: number[], k: number): number {
-    let missingNumCount = 0
-    let idx = 0
+    let left = 0
+    let right = arr.length - 1
     
-    for (let i = 1; i <= arr[arr.length - 1] + k; i++) {
-        if (i !== arr[idx]) {
-            missingNumCount++
-        } else {
-            idx++
-        }
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2)
+        const numMissingAtIdx = arr[mid] - (mid + 1)
         
-        if (missingNumCount === k) {
-            return i
+        if (numMissingAtIdx < k) {
+            left = mid + 1
+        } else {
+            right = mid - 1
         }
     }
+    
+    return left + k
 };
+
+//     let missingNumCount = 0
+//     let idx = 0
+    
+//     for (let i = 1; i <= arr[arr.length - 1] + k; i++) {
+//         if (i !== arr[idx]) {
+//             missingNumCount++
+//         } else {
+//             idx++
+//         }
+        
+//         if (missingNumCount === k) {
+//             return i
+//         }
+//     }
