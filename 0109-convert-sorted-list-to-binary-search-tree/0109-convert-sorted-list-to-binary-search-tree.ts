4225354f5a3130d8340dfa677375dddbe23b6456
Lye Yi Xian <lyeyx999@hotmail.com>
2023-03-13 00:50:43 +0800

    Time: 89 ms (61.54%), Space: 48 MB (76.92%) - LeetHub

---
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.ts
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sortedListToBST(head: ListNode | null): TreeNode | null {
+    return helper(head, null)
+};
+
+function helper(head: ListNode | null, tail: ListNode | null) {
+    if (!head || head === tail) {
+        return null
+    }
+    
+    if (head.next === tail) {
+        return new TreeNode(head.val)
+    }
+    
+    let slow = head
+    let fast = head
+    
+    while (fast !== tail && fast.next !== tail) {
+        slow = slow.next
+        fast = fast.next.next
+    }
+    
+    const root = new TreeNode(slow.val)
+    
+    root.left = helper(head, slow)
+    root.right = helper(slow.next, tail)
+    
+    return root
+}
