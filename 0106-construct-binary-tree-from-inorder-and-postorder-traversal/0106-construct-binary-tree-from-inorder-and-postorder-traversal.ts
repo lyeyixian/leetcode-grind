2846c5b78917edf4b92996a236fad694cfb13362
Lye Yi Xian <lyeyx999@hotmail.com>
2023-03-16 12:50:16 +0800

    Time: 79 ms (92.98%), Space: 46.2 MB (70.18%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.ts
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
+    function findRootIndex(postIndex, inIndexStart, inIndexEnd) {
+        for (let i = inIndexStart; i <= inIndexEnd; i++) {
+            if (inorder[i] === postorder[postIndex]) {
+                return i
+            }
+        }
+    }
+    
+    function build(postIndex, inIndexStart, inIndexEnd) {
+        if (postIndex < 0 || inIndexStart > inIndexEnd) {
+            return null
+        }
+        
+        const inRootIndex = findRootIndex(postIndex, inIndexStart, inIndexEnd)
+        const node = new TreeNode(postorder[postIndex])
+        
+        node.right = build(postIndex - 1, inRootIndex + 1, inIndexEnd)
+        
+        const numOfNodesOnRightSubtree = inIndexEnd - inRootIndex
+        
+        node.left = build(postIndex - (numOfNodesOnRightSubtree + 1), inIndexStart, inRootIndex - 1)
+        
+        return node
+    }
+    
+    return build(postorder.length - 1, 0, inorder.length - 1)
+};
