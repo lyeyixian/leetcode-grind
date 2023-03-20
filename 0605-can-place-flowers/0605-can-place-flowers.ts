596f1c1fe397c13e29a80d21d2619f5b559585b0
Lye Yi Xian <lyeyx999@hotmail.com>
2023-03-20 16:08:32 +0800

    Time: 81 ms (30.86%), Space: 45.2 MB (66.67%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.ts
+++ 0605-can-place-flowers/0605-can-place-flowers.ts
@@ -1,26 +1,9 @@
 function canPlaceFlowers(flowerbed: number[], n: number): boolean {
-    for (let i = 0; i < flowerbed.length; i++) {
-        if (flowerbed[i] === 1) {
-            if (i - 1 >= 0) {
-                flowerbed[i - 1] = -1    
-            }
-            
-            if (i + 1 < flowerbed.length) {
-                flowerbed[i + 1] = -1    
-            }
-        }
-    }
-    
     let count = 0
     
     for (let i = 0; i < flowerbed.length; i++) {
-        if (flowerbed[i] === 0) {
+        if (flowerbed[i] === 0 && flowerbed[i - 1] !== 1 && flowerbed[i + 1] !== 1) {
             flowerbed[i] = 1
-            
-            if (i + 1 < flowerbed.length) {
-                flowerbed[i + 1] = -1
-            }
-            
             count++
         }
     }
