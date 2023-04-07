45ec6b236a972dda217716a50cc1f20f94f6e63b
Lye Yi Xian <lyeyx999@hotmail.com>
2023-04-07 15:35:11 +0800

    Time: 113 ms (55.56%), Space: 55.8 MB (40.74%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.ts
+++ 1020-number-of-enclaves/1020-number-of-enclaves.ts
@@ -1,15 +1,7 @@
 function numEnclaves(grid: number[][]): number {
     const m = grid.length
     const n = grid[0].length
-    
-    return closedIsland(grid)
-};
-
-function closedIsland(grid: number[][]): number {
-    const m = grid.length
-    const n = grid[0].length
     const visited = []
-    let count = 0
     
     for (let i = 0; i < m; i++) {
         visited[i] = new Array(n).fill(false)
@@ -17,15 +9,17 @@ function closedIsland(grid: number[][]): number {
     
     function dfs(i, j) {
         if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === 0 || visited[i][j]) {
-            return 0
+            return
         }
         
         visited[i][j] = true
-        
-        return 1 + dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j - 1) + dfs(i, j + 1)
+        dfs(i - 1, j)
+        dfs(i + 1, j)
+        dfs(i, j - 1)
+        dfs(i, j + 1)
     }
     
-    // visit not closed island first
+    // visit land connected to outer most border
     for (let i = 0; i < m; i++) {
         dfs(i, 0)
         dfs(i, n - 1)
@@ -36,15 +30,68 @@ function closedIsland(grid: number[][]): number {
         dfs(m - 1, j)
     }
     
-    // visit only closed island
+    // count the rest of the land that we haven't visit yet
+    let count = 0
+    
     for (let i = 1; i < m; i++) {
         for (let j = 1; j < n; j++) {
-            const numNodes = dfs(i, j)
-            if (numNodes > 0) {
-                count += numNodes
+            if (grid[i][j] === 1 && !visited[i][j]) {
+                count++
             }
         }
     }
     
     return count
 };
+
+[
+    [0,1,1,0],
+    [0,0,1,0],
+    [0,0,1,0],
+    [0,0,0,0]
+]
+
+// function numEnclaves(grid: number[][]): number {
+//     const m = grid.length
+//     const n = grid[0].length
+//     const visited = []
+    
+//     for (let i = 0; i < m; i++) {
+//         visited[i] = new Array(n).fill(false)
+//     }
+    
+//     function dfs(i, j) {
+//         if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === 0 || visited[i][j]) {
+//             return 0
+//         }
+        
+//         visited[i][j] = true
+        
+//         return 1 + dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j - 1) + dfs(i, j + 1)
+//     }
+    
+//     // visit not closed island first
+//     for (let i = 0; i < m; i++) {
+//         dfs(i, 0)
+//         dfs(i, n - 1)
+//     }
+    
+//     for (let j = 0; j < n; j++) {
+//         dfs(0, j)
+//         dfs(m - 1, j)
+//     }
+    
+//     // visit only closed island
+//     let count = 0
+    
+//     for (let i = 1; i < m; i++) {
+//         for (let j = 1; j < n; j++) {
+//             const numNodes = dfs(i, j)
+//             if (numNodes > 0) {
+//                 count += numNodes
+//             }
+//         }
+//     }
+    
+//     return count
+// };
