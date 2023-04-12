db948492f8445320ffc241eb610310dc90a6cfc4
Lye Yi Xian <lyeyx999@hotmail.com>
2023-04-12 12:36:00 +0800

    Time: 69 ms (74.78%), Space: 44.8 MB (66.09%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.ts
+++ 0071-simplify-path/0071-simplify-path.ts
@@ -1,17 +1,16 @@
 function simplifyPath(path: string): string {
     const stack = []
     
-    path.split('/').filter(dir => dir !== '').forEach(dir => {
-        if (dir === '..') {
-            if (stack.length) {
+    path
+        .split('/')
+        .filter(dir => dir !== '')
+        .forEach(dir => {
+            if (dir !== '..' && dir !== '.') {
+                stack.push(dir)
+            } else if (dir === '..' && stack.length) {
                 stack.pop()
             }
-        } else if (dir === '.') {
-            
-        } else {
-            stack.push(dir)
-        }
-    })
+        })
     
     return '/' + stack.join('/')
 };
