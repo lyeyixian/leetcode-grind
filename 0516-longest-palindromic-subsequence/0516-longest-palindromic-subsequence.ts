7cf192b0e7a9a85a12a788db46e084ca93c3908e
Lye Yi Xian <lyeyx999@hotmail.com>
2023-04-14 14:47:05 +0800

    Time: 457 ms (26.67%), Space: 77.3 MB (53.33%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.ts
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.ts
@@ -1,35 +1,73 @@
 function longestPalindromeSubseq(s: string): number {
-    const reversedS = []
+    let max = 0
+    const mem = []
+    const n = s.length
     
-    for (let i = s.length - 1; i >= 0 ; i--) {
-        reversedS.push(s[i])
+    for (let i = 0; i < n; i++) {
+        mem[i] = new Array(n).fill(-1)
     }
     
-    return longestCommonSubsequence(s, reversedS.join(''))
+    function dp(i, j) {
+        if (i < 0 || j >= s.length) {
+            return 0
+        }
+        
+        if (mem[i][j] !== -1) {
+            return mem[i][j]
+        }
+        
+        if (s[i] === s[j]) {
+            const length = i === j ? 1 : 2
+            
+            mem[i][j] = dp(i - 1, j + 1) + length
+        } else {
+            mem[i][j] = Math.max(dp(i - 1, j), dp(i, j + 1))
+        }
+        
+        return mem[i][j]
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+        dp(i, i)
+        dp(i, i + 1)
+    }
+    
+    return Math.max(...mem.map(arr => Math.max(...arr)))
 };
 
-function longestCommonSubsequence(text1: string, text2: string): number {
-    const dp = []
-    const n = text1.length
-    const m = text2.length
+// use LCS solution to solve this by reversing the string and put it as the 2nd argument
+// function longestPalindromeSubseq(s: string): number {
+//     const reversedS = []
     
-    for (let i = 0; i < n + 1; i++) {
-        dp[i] = []
+//     for (let i = s.length - 1; i >= 0 ; i--) {
+//         reversedS.push(s[i])
+//     }
+    
+//     return longestCommonSubsequence(s, reversedS.join(''))
+// };
+
+// function longestCommonSubsequence(text1: string, text2: string): number {
+//     const dp = []
+//     const n = text1.length
+//     const m = text2.length
+    
+//     for (let i = 0; i < n + 1; i++) {
+//         dp[i] = []
         
-        for (let j = 0; j < m + 1; j++) {
-            dp[i][j] = 0
-        }
-    }
+//         for (let j = 0; j < m + 1; j++) {
+//             dp[i][j] = 0
+//         }
+//     }
     
-    for (let i = 1; i < dp.length; i++) {
-        for (let j = 1; j < dp[0].length; j++) {
-            if (text1[i - 1] === text2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
-            }
-        }
-    }
+//     for (let i = 1; i < dp.length; i++) {
+//         for (let j = 1; j < dp[0].length; j++) {
+//             if (text1[i - 1] === text2[j - 1]) {
+//                 dp[i][j] = dp[i - 1][j - 1] + 1
+//             } else {
+//                 dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+//             }
+//         }
+//     }
     
-    return dp[n][m]
-};
+//     return dp[n][m]
+// };
