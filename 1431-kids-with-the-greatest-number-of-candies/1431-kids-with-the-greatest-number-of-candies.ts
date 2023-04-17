46edf8f7b10eb5f8dc1dbb6dc44a7690d5356462
Lye Yi Xian <lyeyx999@hotmail.com>
2023-04-17 14:38:44 +0800

    Time: 75 ms (21.74%), Space: 44.8 MB (50.54%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
@@ -0,0 +1,7 @@
+function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
+    const max = Math.max(...candies)
+    
+    return candies
+        .map(candy => candy + extraCandies)
+        .map(candy => candy >= max)
+};
