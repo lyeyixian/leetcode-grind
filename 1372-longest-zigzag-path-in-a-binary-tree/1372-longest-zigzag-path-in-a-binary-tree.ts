98585af03db12531733b34b7143e03ae33812411
Lye Yi Xian <lyeyx999@hotmail.com>
2023-04-19 17:08:17 +0800

    Time: 183 ms (100.00%), Space: 65.3 MB (100.00%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.ts
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.ts
@@ -15,24 +15,42 @@
 function longestZigZag(root: TreeNode | null): number {
     let max = 0
     
-    function dfs(node, fromRight, length) {
+    function dfs(node, countFromLeft, countFromRight) {
         if (!node) {
             return
         }
         
-        max = Math.max(max, length)
-        
-        if (fromRight) {
-            dfs(node.left, false, length + 1)
-            dfs(node.right, true, 1)
-        } else {
-            dfs(node.left, false, 1)
-            dfs(node.right, true, length + 1)
-        }
+        max = Math.max(max, countFromLeft, countFromRight)
+        dfs(node.left, countFromRight + 1, 0)
+        dfs(node.right, 0, countFromLeft + 1)
     }
     
-    dfs(root, true, 0)
-    dfs(root, false, 0)
+    dfs(root, 0, 0)
     
     return max
 };
+
+// function longestZigZag(root: TreeNode | null): number {
+//     let max = 0
+    
+//     function dfs(node, fromRight, length) {
+//         if (!node) {
+//             return
+//         }
+        
+//         max = Math.max(max, length)
+        
+//         if (fromRight) {
+//             dfs(node.left, false, length + 1)
+//             dfs(node.right, true, 1)
+//         } else {
+//             dfs(node.left, false, 1)
+//             dfs(node.right, true, length + 1)
+//         }
+//     }
+    
+//     dfs(root, true, 0)
+//     dfs(root, false, 0)
+    
+//     return max
+// };
