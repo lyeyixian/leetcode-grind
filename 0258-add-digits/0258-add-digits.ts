d0b26d712b86b6e8fd387ea276fae10d8beefe29
Lye Yi Xian <lyeyx999@hotmail.com>
2023-04-26 09:46:28 +0800

    Time: 83 ms (27.97%), Space: 45.3 MB (29.37%) - LeetHub

--- 0258-add-digits/0258-add-digits.ts
+++ 0258-add-digits/0258-add-digits.ts
@@ -1,9 +1,25 @@
 function addDigits(num: number): number {
-    let digits = num.toString().split('')
-    
-    while (digits.length > 1) {
-        digits = digits.map(digit => parseInt(digit)).reduce((a, b) => a + b).toString().split('')
+    if (num === 0) {
+        return 0
     }
     
-    return parseInt(digits[0])
+    if (num % 9 === 0) {
+        return 9
+    }
+    
+    return num % 9
 };
+
+// function addDigits(num: number): number {
+//     let digits = num.toString().split('')
+    
+//     while (digits.length > 1) {
+//         digits = digits
+//             .map(digit => parseInt(digit))
+//             .reduce((a, b) => a + b)
+//             .toString()
+//             .split('')
+//     }
+    
+//     return parseInt(digits[0])
+// };
