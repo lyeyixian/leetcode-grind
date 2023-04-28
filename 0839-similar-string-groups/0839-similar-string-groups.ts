6c2a25b413ed2c340e4b10f2287e3f7b03930eee
Lye Yi Xian <lyeyx999@hotmail.com>
2023-04-28 14:11:32 +0800

    Time: 176 ms (100.00%), Space: 46.7 MB (50.00%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.ts
+++ 0839-similar-string-groups/0839-similar-string-groups.ts
@@ -1,49 +1,53 @@
 function numSimilarGroups(strs: string[]): number {
-    const uf = []
+    const n = strs.length
+    const adjList = []
     
-    for (let i = 0; i < strs.length; i++) {
-        uf[i] = i
+    for (let i = 0; i < n; i++) {
+        adjList[i] = []
     }
     
-    for (let i = 0; i < strs.length - 1; i++) {
-        for (let j = i + 1; j < strs.length; j++) {
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = i + 1; j < n; j++) {
             if (isSimilar(strs[i], strs[j])) {
-                union(uf, i, j)
+                adjList[i].push(j)
+                adjList[j].push(i)
             }
         }
     }
     
-    const set = new Set()
+    const visited = new Array(n).fill(false)
     
-    for (const idx of uf) {
-        set.add(find(uf, idx))
+    function dfs(i) {
+        if (visited[i]) {
+            return false
+        }
+        
+        visited[i] = true
+        
+        for (const neighbour of adjList[i]) {
+            dfs(neighbour)
+        }
+        
+        return true
     }
     
-    return set.size
+    let count = 0
+    
+    for (let i = 0; i < n; i++) {
+        if (dfs(i)) {
+            count++
+        }
+    }
+    
+    return count
 };
-
-function union(uf, a, b) {
-    const rootA = find(uf, a)
-    const rootB = find(uf, b)
-    
-    if (rootA !== rootB) {
-        uf[rootA] = rootB    
-    }
-}
-
-function find(uf, a) {
-    if (uf[a] !== a) {
-        uf[a] = find(uf, uf[a])
-    }
-    
-    return uf[a]
-}
 
 function isSimilar(x, y) {
     if (x === y) {
         return true
     }
     
+    // count the index that are different
     let count = 0
     
     for (let i = 0; i < x.length; i++) {
@@ -54,3 +58,62 @@ function isSimilar(x, y) {
     
     return count === 2
 }
+
+// union find solution
+// function numSimilarGroups(strs: string[]): number {
+//     const uf = []
+    
+//     for (let i = 0; i < strs.length; i++) {
+//         uf[i] = i
+//     }
+    
+//     for (let i = 0; i < strs.length - 1; i++) {
+//         for (let j = i + 1; j < strs.length; j++) {
+//             if (isSimilar(strs[i], strs[j])) {
+//                 union(uf, i, j)
+//             }
+//         }
+//     }
+    
+//     const set = new Set()
+    
+//     for (const idx of uf) {
+//         set.add(find(uf, idx))
+//     }
+    
+//     return set.size
+// };
+
+// function union(uf, a, b) {
+//     const rootA = find(uf, a)
+//     const rootB = find(uf, b)
+    
+//     if (rootA !== rootB) {
+//         uf[rootA] = rootB    
+//     }
+// }
+
+// function find(uf, a) {
+//     if (uf[a] !== a) {
+//         uf[a] = find(uf, uf[a])
+//     }
+    
+//     return uf[a]
+// }
+
+// function isSimilar(x, y) {
+//     if (x === y) {
+//         return true
+//     }
+    
+//     // count the index that are different
+//     let count = 0
+    
+//     for (let i = 0; i < x.length; i++) {
+//         if (x[i] !== y[i]) {
+//             count++
+//         }
+//     }
+    
+//     return count === 2
+// }
