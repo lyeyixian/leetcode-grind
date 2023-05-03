85a83a5d12ff59d23bae57bbe1b8aa0703275208
Lye Yi Xian <lyeyx999@hotmail.com>
2023-05-03 21:43:14 +0800

    Time: 103 ms (49.27%), Space: 50.1 MB (43.48%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.ts
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.ts
@@ -1,42 +1,73 @@
 function findDifference(nums1: number[], nums2: number[]): number[][] {
-    nums1.sort((a, b) => a - b)
-    nums2.sort((a, b) => a - b)
+    const set1 = new Set()
+    const set2 = new Set()
     
-    const newNums1 = removeDuplicate(nums1)
-    const newNums2 = removeDuplicate(nums2)
-    let ptr1 = 0
-    let ptr2 = 0
+    for (const num of nums1) {
+        set1.add(num)
+    }
     
-    while (ptr1 < newNums1.length && ptr2 < newNums2.length) {
-        const num1 = newNums1[ptr1]
-        const num2 = newNums2[ptr2]
-        
-        if (num1 < num2) {
-            ptr1++
-        } else if (num1 > num2) {
-            ptr2++
-        } else {
-            while (ptr1 < newNums1.length && newNums1[ptr1] === num1) {
-                newNums1[ptr1] = null
-                ptr1++    
-            }
-            
-            while (ptr2 < newNums2.length && newNums2[ptr2] === num2) {
-                newNums2[ptr2] = null
-                ptr2++
-            }
+    for (const num of nums2) {
+        set2.add(num)
+    }
+    
+    const res1 = []
+    
+    for (const num of set1.values()) {
+        if (!set2.has(num)) {
+            res1.push(num)
         }
     }
     
-    return [newNums1.filter(num => num !== null), newNums2.filter(num => num !== null)]
+    const res2 = []
+    
+    for (const num of set2.values()) {
+        if (!set1.has(num)) {
+            res2.push(num)
+        }
+    }
+    
+    return [res1, res2]
 };
 
-function removeDuplicate(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i - 1] === arr[i]) {
-            arr[i - 1] = null
-        }
-    }
+// function findDifference(nums1: number[], nums2: number[]): number[][] {
+//     nums1.sort((a, b) => a - b)
+//     nums2.sort((a, b) => a - b)
     
-    return arr.filter(num => num !== null)
-}
+//     const newNums1 = removeDuplicate(nums1)
+//     const newNums2 = removeDuplicate(nums2)
+//     let ptr1 = 0
+//     let ptr2 = 0
+    
+//     while (ptr1 < newNums1.length && ptr2 < newNums2.length) {
+//         const num1 = newNums1[ptr1]
+//         const num2 = newNums2[ptr2]
+        
+//         if (num1 < num2) {
+//             ptr1++
+//         } else if (num1 > num2) {
+//             ptr2++
+//         } else {
+//             while (ptr1 < newNums1.length && newNums1[ptr1] === num1) {
+//                 newNums1[ptr1] = null
+//                 ptr1++    
+//             }
+            
+//             while (ptr2 < newNums2.length && newNums2[ptr2] === num2) {
+//                 newNums2[ptr2] = null
+//                 ptr2++
+//             }
+//         }
+//     }
+    
+//     return [newNums1.filter(num => num !== null), newNums2.filter(num => num !== null)]
+// };
+
+// function removeDuplicate(arr) {
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i - 1] === arr[i]) {
+//             arr[i - 1] = null
+//         }
+//     }
+    
+//     return arr.filter(num => num !== null)
+// }
