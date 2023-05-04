e77b11779826b2a06144fce95abb0306f3c11f99
Lye Yi Xian <lyeyx999@hotmail.com>
2023-05-04 10:36:47 +0800

    Time: 68 ms (93.33%), Space: 47.2 MB (53.33%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.ts
+++ 0649-dota2-senate/0649-dota2-senate.ts
@@ -1,36 +1,63 @@
 function predictPartyVictory(senate: string): string {
-    function ban(char, fromIndex) {
-        while (true) {
-            if (senate[fromIndex] === char && !banned[fromIndex]) {
-                banned[fromIndex] = true
-                return
-            }
-            
-            fromIndex++
-            fromIndex %= n
-        }
-    }
-    
     const n = senate.length
-    const banned = new Array(n).fill(false)
-    let radiantCount = senate.split('').filter(char => char === 'R').length
-    let direCount = n - radiantCount
-    let index = 0
+    const rQueue = []
+    const dQueue = []
     
-    while (radiantCount > 0 && direCount > 0) {
-        if (!banned[index]) {
-            if (senate[index] === 'R') {
-                ban('D', (index + 1) % n)
-                direCount--
-            } else {
-                ban('R', (index + 1) % n)
-                radiantCount--
-            }    
+    for (let i = 0; i < n; i++) {
+        if (senate[i] === 'R') {
+            rQueue.push(i)
+        } else {
+            dQueue.push(i)
         }
-        
-        index++
-        index %= n
     }
     
-    return direCount === 0 ? 'Radiant' : 'Dire'
+    while (rQueue.length && dQueue.length) {
+        const rIdx = rQueue.shift()
+        const qIdx = dQueue.shift()
+        
+        if (rIdx < qIdx) {
+            rQueue.push(rIdx + n)
+        } else {
+            dQueue.push(qIdx + n)
+        }
+    }
+    
+    return rQueue.length ? 'Radiant' : 'Dire'
 }
+
+// function predictPartyVictory(senate: string): string {
+//     function ban(char, fromIndex) {
+//         while (true) {
+//             if (senate[fromIndex] === char && !banned[fromIndex]) {
+//                 banned[fromIndex] = true
+//                 return
+//             }
+            
+//             fromIndex++
+//             fromIndex %= n
+//         }
+//     }
+    
+//     const n = senate.length
+//     const banned = new Array(n).fill(false)
+//     let radiantCount = senate.split('').filter(char => char === 'R').length
+//     let direCount = n - radiantCount
+//     let index = 0
+    
+//     while (radiantCount > 0 && direCount > 0) {
+//         if (!banned[index]) {
+//             if (senate[index] === 'R') {
+//                 ban('D', (index + 1) % n)
+//                 direCount--
+//             } else {
+//                 ban('R', (index + 1) % n)
+//                 radiantCount--
+//             }    
+//         }
+        
+//         index++
+//         index %= n
+//     }
+    
+//     return direCount === 0 ? 'Radiant' : 'Dire'
+// }
