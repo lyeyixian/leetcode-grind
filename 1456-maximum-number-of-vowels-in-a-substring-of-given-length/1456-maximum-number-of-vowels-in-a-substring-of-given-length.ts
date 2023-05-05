3842940787f16bb627b2ca7b663500aa41716dc8
Lye Yi Xian <lyeyx999@hotmail.com>
2023-05-05 22:53:35 +0800

    Time: 101 ms (66.67%), Space: 46.5 MB (68.75%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.ts
@@ -0,0 +1,37 @@
+function maxVowels(s: string, k: number): number {
+    let count = 0
+    let left = 0
+    let right = k - 1
+    const vowel = new Set()
+    
+    vowel.add('a')
+    vowel.add('e')
+    vowel.add('i')
+    vowel.add('o')
+    vowel.add('u')
+    
+    for (let i = 0; i < k; i++) {
+        if (vowel.has(s[i])) {
+            count++
+        }
+    }
+    
+    let res = count
+    
+    while (right < s.length) {
+        if (vowel.has(s[left])) {
+            count--
+        }
+        
+        left++
+        right++
+        
+        if (vowel.has(s[right])) {
+            count++
+        }
+        
+        res = Math.max(res, count)
+    }
+    
+    return res
+};
