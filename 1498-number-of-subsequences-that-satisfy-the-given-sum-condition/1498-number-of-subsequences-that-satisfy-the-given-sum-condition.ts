3e57d0ac7b30ef6623bf3dfaa9fa78efffabdcd5
Lye Yi Xian <lyeyx999@hotmail.com>
2023-05-06 11:09:43 +0800

    Time: 211 ms (33.33%), Space: 59.2 MB (33.33%) - LeetHub

---
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.ts
@@ -0,0 +1,26 @@
+function numSubseq(nums: number[], target: number): number {
+    nums.sort((a, b) => a - b)
+    
+    const modulo = 1000000007
+    const power = [1]
+    
+    for (let i = 1; i < nums.length; i++) {
+        power[i] = (power[i - 1] * 2) % modulo
+    }
+    
+    let res = 0
+    let left = 0
+    let right = nums.length - 1
+    
+    while (left <= right) {
+        if (nums[left] + nums[right] <= target) {
+            res += (power[right - left] % modulo)
+            res %= modulo
+            left++
+        } else {
+            right--
+        }
+    }
+    
+    return res
+};
