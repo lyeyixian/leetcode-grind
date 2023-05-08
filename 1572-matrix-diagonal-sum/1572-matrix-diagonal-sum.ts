98a198345650b97599d0d97f354a742047b5fe52
Lye Yi Xian <lyeyx999@hotmail.com>
2023-05-08 12:47:08 +0800

    Time: 54 ms (95.28%), Space: 44.4 MB (93.40%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.ts
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.ts
@@ -1,19 +1,15 @@
 function diagonalSum(mat: number[][]): number {
     let sum = 0
     const n = mat.length
-    let primaryCol = 0
     
     for (let i = 0; i < n; i++) {
-        sum += mat[i][primaryCol]
-        mat[i][primaryCol] = 0
-        primaryCol++
-    }
-    
-    let secondaryCol = n - 1
-    for (let i = 0; i < n; i++) {
-        sum += mat[i][secondaryCol]
-        mat[i][secondaryCol] = 0
-        secondaryCol--
+        // primary diagonal
+        sum += mat[i][i]
+        mat[i][i] = 0
+        
+        // secondary diagonal
+        sum += mat[i][n - 1 - i]
+        mat[i][n - 1 - i] = 0
     }
     
     return sum
