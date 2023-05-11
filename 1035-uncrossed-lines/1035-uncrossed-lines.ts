bd191c597f7d271b011f25a7430970f5442f26db
Lye Yi Xian <lyeyx999@hotmail.com>
2023-05-11 12:18:23 +0800

    Time: 107 ms (25.00%), Space: 49.3 MB (25.00%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.ts
+++ 1035-uncrossed-lines/1035-uncrossed-lines.ts
@@ -22,13 +22,12 @@ function maxUncrossedLines(nums1: number[], nums2: number[]): number {
         
         let res = 0
         
-        for (let k = j; k < m; k++) {
-            if (nums2[k] === nums1[i]) {
-                res = Math.max(res, 1 + dp(i + 1, k + 1))
-            }
+        if (nums1[i] === nums2[j]) {
+            res = 1 + dp(i + 1, j + 1)
+        } else {
+            res = Math.max(dp(i, j + 1), dp(i + 1, j))
         }
         
-        res = Math.max(res, dp(i + 1, j))
         mem[i][j] = res
         
         return res
@@ -36,3 +35,42 @@ function maxUncrossedLines(nums1: number[], nums2: number[]): number {
     
     return dp(0, 0)
 };
+
+// function maxUncrossedLines(nums1: number[], nums2: number[]): number {
+//     const mem = []
+//     const n = nums1.length
+//     const m = nums2.length
+    
+//     for (let i = 0; i < n; i++) {
+//         mem[i] = []
+        
+//         for (let j = 0; j < m; j++) {
+//             mem[i][j] = -1
+//         }
+//     }
+    
+//     function dp(i, j) {
+//         if (i >= n || j >= m) {
+//             return 0
+//         }
+        
+//         if (mem[i][j] !== -1) {
+//             return mem[i][j]
+//         }
+        
+//         let res = 0
+        
+//         for (let k = j; k < m; k++) {
+//             if (nums2[k] === nums1[i]) {
+//                 res = Math.max(res, 1 + dp(i + 1, k + 1))
+//             }
+//         }
+        
+//         res = Math.max(res, dp(i + 1, j))
+//         mem[i][j] = res
+        
+//         return res
+//     }
+    
+//     return dp(0, 0)
+// };
