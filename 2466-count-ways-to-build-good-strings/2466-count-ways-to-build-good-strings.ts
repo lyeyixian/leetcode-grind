2b24a2a9d1341d0fabc3764f779daa3671d13982
Lye Yi Xian <lyeyx999@hotmail.com>
2023-05-13 23:15:40 +0800

    Time: 167 ms (100.00%), Space: 57.3 MB (100.00%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.ts
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.ts
@@ -1,24 +1,39 @@
 function countGoodStrings(low: number, high: number, zero: number, one: number): number {
     const modulo = 1000000007
-    const mem = new Map()
+    const dp = new Array(high + 1).fill(0)
     
-    function dp(len) {
-        if (len > high) {
-            return 0
-        }
-        
-        if (mem.has(len)) {
-            return mem.get(len)
-        }
-        
-        let res = len >= low ? 1 : 0
-        res += dp(len + zero) + dp(len + one)
-        res %= modulo
-        
-        mem.set(len, res)
-        
-        return res
+    dp[0] = 1
+    
+    for (let i = 1; i <= high; i++) {
+        dp[i] += dp[i - zero] || 0
+        dp[i] += dp[i - one] || 0
+        dp[i] %= modulo
     }
     
-    return dp(0)
+    return dp.slice(low, high + 1).reduce((a, b) => a + b) % modulo
 };
+
+// function countGoodStrings(low: number, high: number, zero: number, one: number): number {
+//     const modulo = 1000000007
+//     const mem = new Map()
+    
+//     function dp(len) {
+//         if (len > high) {
+//             return 0
+//         }
+        
+//         if (mem.has(len)) {
+//             return mem.get(len)
+//         }
+        
+//         let res = len >= low ? 1 : 0
+//         res += dp(len + zero) + dp(len + one)
+//         res %= modulo
+        
+//         mem.set(len, res)
+        
+//         return res
+//     }
+    
+//     return dp(0)
+// };
