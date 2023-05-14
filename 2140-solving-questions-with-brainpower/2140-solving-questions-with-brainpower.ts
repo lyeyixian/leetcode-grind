f80c3bf6a9928d91ac31f8df649da3da6f3ce3ad
Lye Yi Xian <lyeyx999@hotmail.com>
2023-05-14 22:08:18 +0800

    Time: 165 ms (69.88%), Space: 77.6 MB (86.75%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.ts
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.ts
@@ -1,22 +1,35 @@
 function mostPoints(questions: number[][]): number {
     const n = questions.length
-    const mem = new Array(n).fill(-1)
+    const dp = new Array(n).fill(0)
     
-    function dp(i) {
-        if (i >= n) {
-            return 0
-        }
-        
-        if (mem[i] !== -1) {
-            return mem[i]
-        }
-        
+    for (let i = n - 1; i >= 0; i--) {
         const [points, toSkip] = questions[i]
         
-        mem[i] = Math.max(points + dp(i + 1 + toSkip), dp(i + 1))
-        
-        return mem[i]
+        dp[i] = Math.max(points + (dp[i + 1 + toSkip] || 0), dp[i + 1] || 0)
     }
     
-    return dp(0)
+    return dp[0]
 };
+
+// function mostPoints(questions: number[][]): number {
+//     const n = questions.length
+//     const mem = new Array(n).fill(-1)
+    
+//     function dp(i) {
+//         if (i >= n) {
+//             return 0
+//         }
+        
+//         if (mem[i] !== -1) {
+//             return mem[i]
+//         }
+        
+//         const [points, toSkip] = questions[i]
+        
+//         mem[i] = Math.max(points + dp(i + 1 + toSkip), dp(i + 1))
+        
+//         return mem[i]
+//     }
+    
+//     return dp(0)
+// };
