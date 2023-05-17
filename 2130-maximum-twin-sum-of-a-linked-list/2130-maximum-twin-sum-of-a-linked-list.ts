c3f0639e7bb82e73eb6193a9e6ae8850f23cf0f3
Lye Yi Xian <lyeyx999@hotmail.com>
2023-05-17 12:53:03 +0800

    Time: 127 ms (92.96%), Space: 66.9 MB (81.69%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.ts
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.ts
@@ -11,45 +11,79 @@
  */
 
 function pairSum(head: ListNode | null): number {
-    let curr = head
-    let n = 1
+    // slow fast to find middle point
+    let slow = head
+    let fast = head
+    let prev = null
     
-    while (curr.next) {
-        curr = curr.next
-        n++
+    while (fast && fast.next) {
+        // advance fast pointer first cos later will be reversed
+        fast = fast.next.next
+        
+        // reverse in the meantime
+        const temp = slow.next
+        
+        slow.next = prev
+        prev = slow
+        
+        // advance slow pointer
+        slow = temp
     }
     
-    let right = head
-    
-    for (let i = 0; i < Math.floor(n / 2); i++) {
-        right = right.next
-    }
-    
-    right = reverse(right)
-    
-    let left = head
+    let left = prev
+    let right = slow
     let max = 0
     
-    for (let i = 0; i < Math.floor(n / 2); i++) {
+    while (right) {
         max = Math.max(max, left.val + right.val)
         left = left.next
         right = right.next
     }
     
     return max
-};
-
-function reverse(head) {
-    let prev = null
-    let curr = head
-    
-    while (curr) {
-        const temp = curr.next
-        
-        curr.next = prev
-        prev = curr
-        curr = temp
-    }
-    
-    return prev
 }
+
+// function pairSum(head: ListNode | null): number {
+//     let curr = head
+//     let n = 1
+    
+//     while (curr.next) {
+//         curr = curr.next
+//         n++
+//     }
+    
+//     let right = head
+    
+//     for (let i = 0; i < Math.floor(n / 2); i++) {
+//         right = right.next
+//     }
+    
+//     right = reverse(right)
+    
+//     let left = head
+//     let max = 0
+    
+//     for (let i = 0; i < Math.floor(n / 2); i++) {
+//         max = Math.max(max, left.val + right.val)
+//         left = left.next
+//         right = right.next
+//     }
+    
+//     return max
+// };
+
+// // recursive solution TLE so need iterative solution
+// function reverse(head) {
+//     let prev = null
+//     let curr = head
+    
+//     while (curr) {
+//         const temp = curr.next
+        
+//         curr.next = prev
+//         prev = curr
+//         curr = temp
+//     }
+    
+//     return prev
+// }
