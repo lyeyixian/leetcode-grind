d8877f5b08387bc56748291c1c1eaa37391adfe3
Lye Yi Xian <lyeyx999@hotmail.com>
2023-05-20 15:02:37 +0800

    Time: 54 ms (92.86%), Space: 44.5 MB (35.71%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.ts
+++ 0399-evaluate-division/0399-evaluate-division.ts
@@ -1,19 +1,28 @@
 function calcEquation(equations: string[][], values: number[], queries: string[][]): number[] {
-    function dfs(node, target, res, visited) {
-        if (node === target) {
-            return res
+    function bfs(node, target) {
+        if (!adjList[node] || !adjList[target]) {
+            return -1.0
         }
+        
+        const queue = [[node, 1]]
+        const visited = new Array(n).fill(false)
         
         visited[node] = true
         
-        for (const { id, value } of adjList[node]) {
-            if (visited[id]) {
-                continue
+        while (queue.length) {
+            const [curr, sum] = queue.shift()
+            
+            if (curr === target) {
+                return sum
             }
             
-            const ans = dfs(id, target, value * res, visited)
-            if (ans !== -1) {
-                return ans
+            for (const { id, value } of adjList[curr]) {
+                if (visited[id]) {
+                    continue
+                }
+                
+                visited[id] = true
+                queue.push([id, sum * value])
             }
         }
         
@@ -42,13 +51,64 @@ function calcEquation(equations: string[][], values: number[], queries: string[]
     const res = []
     
     for (const [from, to] of queries) {
-        if (!adjList[from] || !adjList[to]) {
-            res.push(-1.0)
-            continue
-        }
-        
-        res.push(dfs(from, to, 1, new Array(n).fill(false)))
+        res.push(bfs(from, to))
     }
     
     return res
 };
+
+// dfs
+// function calcEquation(equations: string[][], values: number[], queries: string[][]): number[] {
+//     function dfs(node, target, res, visited) {
+//         if (node === target) {
+//             return res
+//         }
+        
+//         visited[node] = true
+        
+//         for (const { id, value } of adjList[node]) {
+//             if (visited[id]) {
+//                 continue
+//             }
+            
+//             const ans = dfs(id, target, value * res, visited)
+//             if (ans !== -1) {
+//                 return ans
+//             }
+//         }
+        
+//         return -1.0
+//     }
+    
+//     const adjList = {}
+//     const n = equations.length
+    
+//     for (let i = 0; i < n; i++) {
+//         const [u, v] = equations[i]
+//         const value = values[i]
+        
+//         if (!adjList[u]) {
+//             adjList[u] = []
+//         }
+        
+//         if (!adjList[v]) {
+//             adjList[v] = []
+//         }
+        
+//         adjList[u].push({ id: v, value })
+//         adjList[v].push({ id: u, value: 1 / value })
+//     }
+    
+//     const res = []
+    
+//     for (const [from, to] of queries) {
+//         if (!adjList[from] || !adjList[to]) {
+//             res.push(-1.0)
+//             continue
+//         }
+        
+//         res.push(dfs(from, to, 1, new Array(n).fill(false)))
+//     }
+    
+//     return res
+// };
