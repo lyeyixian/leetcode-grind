e769fa5eeb1b5146434f093de466a25478bfaf64
Lye Yi Xian <lyeyx999@hotmail.com>
2023-05-29 14:27:50 +0800

    Time: 133 ms (64.03%), Space: 50.2 MB (72.81%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.ts
+++ 1603-design-parking-system/1603-design-parking-system.ts
@@ -1,36 +1,16 @@
 class ParkingSystem {
-    big
-    medium
-    small
+    arr
     
     constructor(big: number, medium: number, small: number) {
-        this.big = big
-        this.medium = medium
-        this.small = small
+        this.arr = [big, medium, small]
     }
 
     addCar(carType: number): boolean {
-        if (carType === 1) {
-            if (this.big === 0) {
-                return false
-            } else {
-                this.big--
-                return true
-            }
-        } else if (carType === 2) {
-            if (this.medium === 0) {
-                return false
-            } else {
-                this.medium--
-                return true
-            }
+        if (this.arr[carType - 1] === 0) {
+            return false
         } else {
-            if (this.small === 0) {
-                return false
-            } else {
-                this.small--
-                return true
-            }
+            this.arr[carType - 1]--
+            return true
         }
     }
 }
