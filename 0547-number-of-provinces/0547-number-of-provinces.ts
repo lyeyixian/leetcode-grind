80923e96c5daa532b6b9c512774a4edf125ef0ab
Lye Yi Xian <lyeyx999@hotmail.com>
2023-06-04 11:11:57 +0800

    Time: 72 ms (76.06%), Space: 45.7 MB (52.11%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.ts
+++ 0547-number-of-provinces/0547-number-of-provinces.ts
@@ -1,42 +1,88 @@
-function findCircleNum(isConnected: number[][]): number {
-    const n = isConnected.length
-    const adjList = []
+class UnionFind {
+    arr
     
-    for (let i = 0; i < n; i++) {
-        adjList[i] = []
+    constructor(n) {
+        this.arr = []
+        
+        for (let i = 0; i < n; i++) {
+            this.arr[i] = i
+        }
     }
     
+    union(u, v) {
+        const rootU = this.find(u)
+        const rootV = this.find(v)
+        
+        if (rootU !== rootV) {
+            this.arr[rootU] = rootV    
+        }
+    }
+    
+    find(u) {
+        if (this.arr[u] !== u) {
+            this.arr[u] = this.find(this.arr[u])
+        }
+        
+        return this.arr[u]
+    }
+}
+
+function findCircleNum(isConnected: number[][]): number {
+    const n = isConnected.length
+    const uf = new UnionFind(n)
+    let numOfComponents = n
+    
     for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (isConnected[i][j]) {
-                adjList[i].push(j)
+        for (let j = i + 1; j < n; j++) {
+            if (isConnected[i][j] && uf.find(i) !== uf.find(j)) {
+                numOfComponents--
+                uf.union(i, j)
             }
         }
     }
-    
-    const visited = new Array(n).fill(false)
-    
-    function dfs(i) {
-        if (i >= n || visited[i]) {
-            return false
-        }
         
-        visited[i] = true
+    return numOfComponents
+}
+
+// function findCircleNum(isConnected: number[][]): number {
+//     const n = isConnected.length
+//     const adjList = []
+    
+//     for (let i = 0; i < n; i++) {
+//         adjList[i] = []
+//     }
+    
+//     for (let i = 0; i < n; i++) {
+//         for (let j = 0; j < n; j++) {
+//             if (isConnected[i][j]) {
+//                 adjList[i].push(j)
+//             }
+//         }
+//     }
+    
+//     const visited = new Array(n).fill(false)
+    
+//     function dfs(i) {
+//         if (i >= n || visited[i]) {
+//             return false
+//         }
         
-        for (const neighbour of adjList[i]) {
-            dfs(neighbour)
-        }
+//         visited[i] = true
         
-        return true
-    }
+//         for (const neighbour of adjList[i]) {
+//             dfs(neighbour)
+//         }
+        
+//         return true
+//     }
     
-    let count = 0
+//     let count = 0
     
-    for (let i = 0; i < n; i++) {
-        if (dfs(i)) {
-            count++
-        }
-    }
+//     for (let i = 0; i < n; i++) {
+//         if (dfs(i)) {
+//             count++
+//         }
+//     }
     
-    return count
-};
+//     return count
+// };
