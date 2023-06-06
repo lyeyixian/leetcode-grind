ac8659fe0ea5c98356365afc280fd6663a9bf5fe
Lye Yi Xian <lyeyx999@hotmail.com>
2023-06-06 11:06:02 +0800

    Time: 66 ms (49.31%), Space: 45.2 MB (20.55%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.ts
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.ts
@@ -1,13 +1,43 @@
 function canMakeArithmeticProgression(arr: number[]): boolean {
-    arr.sort((a, b) => a - b)
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
+    const n = arr.length
     
-    const diff = arr[1] - arr[0]
-    
-    for (let i = 2; i < arr.length; i++) {
-        if (arr[i] - arr[i - 1] !== diff) {
-            return false
-        }
+    // all numbers are equal, so diff is the same for all
+    if (max - min === 0) {
+        return true
     }
     
-    return true
-};
+    // can form arithmetic progression
+    if ((max - min) % (n - 1) !== 0) {
+        return false
+    }
+    
+    const diff = (max - min) / (n - 1)
+    const set = new Set()
+    
+    for (const num of arr) {
+        if ((num - min) % diff !== 0) {
+            return false
+        }
+        
+        set.add(num)
+    }
+    
+    // set is to check if arr is distinct
+    return set.size === n
+}
+
+// function canMakeArithmeticProgression(arr: number[]): boolean {
+//     arr.sort((a, b) => a - b)
+    
+//     const diff = arr[1] - arr[0]
+    
+//     for (let i = 2; i < arr.length; i++) {
+//         if (arr[i] - arr[i - 1] !== diff) {
+//             return false
+//         }
+//     }
+    
+//     return true
+// };
