a661df415c887e6ed450a81d74e1de9b8663c20d
Lye Yi Xian <lyeyx999@hotmail.com>
2023-06-07 14:37:02 +0800

    Time: 58 ms (25.00%), Space: 42.9 MB (50.00%) - LeetHub

---
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.ts
@@ -0,0 +1,52 @@
+function minFlips(a: number, b: number, c: number): number {
+    function toBinaryStr(num) {
+        return num.toString(2)
+    }
+    
+    const arrA = toBinaryStr(a).split('')
+    const lenA = arrA.length
+    const arrB = toBinaryStr(b).split('')
+    const lenB = arrB.length
+    const arrC = toBinaryStr(c).split('')
+    const lenC = arrC.length
+    const maxLength = Math.max(lenA, lenB, lenC)
+    
+    if (lenA < maxLength) {
+        const diff = maxLength - lenA
+        arrA.unshift(...'0'.repeat(diff).split(''))
+    }
+    
+    if (lenB < maxLength) {
+        const diff = maxLength - lenB
+        arrB.unshift(...'0'.repeat(diff).split(''))
+    }
+    
+    if (lenC < maxLength) {
+        const diff = maxLength - lenC
+        arrC.unshift(...'0'.repeat(diff).split(''))
+    }
+    
+    let count = 0
+    
+    for (let i = 0; i < maxLength; i++) {
+        const bitA = arrA[i]
+        const bitB = arrB[i]
+        const bitC = arrC[i]
+        
+        if (bitC === '1') {
+            if (bitA === '0' && bitB === '0') {
+                count++    
+            }
+        } else {
+            if (bitA === '1') {
+                count++
+            }
+            
+            if (bitB === '1') {
+                count++
+            }
+        }
+    }
+    
+    return count
+};
