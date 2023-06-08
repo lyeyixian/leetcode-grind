f06c023988aad07b7bc809a91415f4661bcd1f9b
Lye Yi Xian <lyeyx999@hotmail.com>
2023-06-08 19:02:47 +0800

    Time: 70 ms (42.55%), Space: 44.3 MB (98.94%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.ts
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.ts
@@ -1,26 +1,43 @@
 function countNegatives(grid: number[][]): number {
     const n = grid[0].length
     let count = 0
+    let rightBorder = n - 1
     
     for (const row of grid) {
-        let left = 0
-        let right = n - 1
-        
-        while (left <= right) {
-            const mid = Math.floor((left + right) / 2)
-            
-            if (row[mid] < 0) {
-                right = mid - 1
-            } else {
-                left = mid + 1
-            }
+        while (rightBorder >= 0 && row[rightBorder] < 0) {
+            rightBorder--
         }
         
-        count += n - left
+        count += n - (rightBorder + 1)
     }
     
     return count
 }
+
+// binary search
+// function countNegatives(grid: number[][]): number {
+//     const n = grid[0].length
+//     let count = 0
+    
+//     for (const row of grid) {
+//         let left = 0
+//         let right = n - 1
+        
+//         while (left <= right) {
+//             const mid = Math.floor((left + right) / 2)
+            
+//             if (row[mid] < 0) {
+//                 right = mid - 1
+//             } else {
+//                 left = mid + 1
+//             }
+//         }
+        
+//         count += n - left
+//     }
+    
+//     return count
+// }
 
 // brute force
 // function countNegatives(grid: number[][]): number {
