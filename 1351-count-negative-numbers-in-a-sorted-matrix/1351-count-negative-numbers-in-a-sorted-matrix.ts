2a0bd0c8c14b8a5d6b3016b95347ad33a56a8d3a
Lye Yi Xian <lyeyx999@hotmail.com>
2023-06-08 18:40:02 +0800

    Time: 62 ms (73.40%), Space: 45.5 MB (31.91%) - LeetHub

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.ts
@@ -0,0 +1,15 @@
+function countNegatives(grid: number[][]): number {
+    const m = grid.length
+    const n = grid[0].length
+    let count = 0
+    
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] < 0) {
+                count++
+            }
+        }
+    }
+    
+    return count
+};
