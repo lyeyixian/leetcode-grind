2f4554cb71dfc447620af8a9e33221cc74f0b2ac
Lye Yi Xian <lyeyx999@hotmail.com>
2023-06-10 13:08:25 +0800

    Time: 63 ms (100.00%), Space: 45.2 MB (100.00%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.ts
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.ts
@@ -4,20 +4,16 @@ function maxValue(n: number, index: number, maxSum: number): number {
         
         // lhs
         if (target > index) {
-            // sum += apSum(target - index, target, index + 1)
-            sum += Math.floor((target + target - index) * (index + 1) / 2)
+            sum += apSum(target - index, target, index + 1)
         } else {
-            // sum += apSum(1, target, target) + index - target + 1
-            sum += Math.floor((target + 1) * target / 2 + index - target + 1)
+            sum += apSum(1, target, target) + index - target + 1
         }
         
         // rhs
         if (target >= n - index) {
-            // sum += apSum(target - (n - index) + 1, target, n - index)
-            sum += Math.floor((target + target - n + 1 + index) * (n - index) / 2)
+            sum += apSum(target - (n - index) + 1, target, n - index)
         } else {
-            // sum += apSum(1, target, target) + n - index - target
-            sum += Math.floor((target + 1) * target / 2 + n - index - target)
+            sum += apSum(1, target, target) + n - index - target
         }
         
         // remove double count
@@ -41,5 +37,5 @@ function maxValue(n: number, index: number, maxSum: number): number {
 };
 
 function apSum(a1, an, n) {
-    return (a1 + an) * Math.floor(n / 2)
+    return Math.floor((a1 + an) * n / 2)
 }
